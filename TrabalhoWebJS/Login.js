40cf8db1f89a409e2167bab87fd03bcdff901a60
Rodrigo Goto <96962958+Gotoxico@users.noreply.github.com>
2023-11-07 16:09:39 -0300

    Mudanças importantes 3

--- TrabalhoWebJS/Login.js
+++ TrabalhoWebJS/Login.js
@@ -1,8 +1,17 @@
-function validar_login(username, password, validator){
+function validar_login(username, password, validator, age){
     let user = username.value
     let senha = password.value
     let validacao = validator.value
+    let atual = new Date()
+    let hora_atual = atual.getHours() + ":" + atual.getMinutes()
+    let idade = parseint(age.value)
 
-    if(user=="admin" && senha=="admin" && validacao == "")
-
+    if(user==="admin" && senha==="admin" && validacao === hora_atual && idade>=18){
+        window.location.href="../TrabalhoWebHTML/TrabalhoWebHome.html"
+    }
+    else{
+        alert("Falhou")
+    }
 }
+
+
