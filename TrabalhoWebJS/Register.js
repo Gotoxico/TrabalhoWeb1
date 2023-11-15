3a7dcf7528b965c6dc61ccb46a45fc64656c0578
Rodrigo Goto <96962958+Gotoxico@users.noreply.github.com>
2023-11-15 14:23:26 -0300

    Mudanças 4

--- TrabalhoWebJS/Register.js
+++ TrabalhoWebJS/Register.js
@@ -1,8 +1,35 @@
+function cpf(cpf){
+    cpf = cpf.replace(/\D/g, '');
+    if(cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+    var result = true;
+    [9,10].forEach(function(j){
+        var soma = 0, r;
+        cpf.split(/(?=)/).splice(0,j).forEach(function(e, i){
+            soma += parseInt(e) * ((j+2)-(i+1));
+        });
+        r = soma % 11;
+        r = (r <2)?0:11-r;
+        if(r != cpf.substring(j, j+1)) result = false;
+    });
+    return result;
+}
+
+function validarSenha() {
+    senha = document.getElementsByName('senha')[0].value;
+    senhaC = document.getElementsByName('senhaC')[0].value;
+  
+    return senha === senhaC;
+  }
+
 function validar_cadastro(campo1){
     let cad = campo1.value
-    let tamanho = cad.length
-    if(tamanho==11){
+    let cpf_valido = cpf(cad)
+    let senha_valida = validarSenha();
+
+    if(cpf_valido == true && senha_valida == true){
         alert("Cadastrado!")
         window.location.href="../TrabalhoWebHTML/Login.html"
+    }else{
+        alert("Cadastro inválido!")
     }
 }
